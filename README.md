1477a3ac9f8a259f290cb5d0104d8ef488075cf4
Adrien Pavao <adrien.pavao@gmail.com>
2019-03-15 02:16:05 +0100

    Experiments.

--- README.md
+++ README.md
@@ -9,3 +9,17 @@
 * [medGAN](https://github.com/mp2893/medgan)
 * [Copula](https://github.com/Didayolo/medi-chal/tree/master/code/generators)
 * [Random Forest Imputations](https://github.com/Didayolo/medi-chal/tree/master/code/generators)
+
+
+### Set-up VAE benchmark
+
+```
+git clone http://github.com/didayolo/autodata
+cd autodata
+pip install -r requirements.txt
+cd ..
+git clone http://github.com/didayolo/generators
+cd generators
+pip install -r requirements.txt
+jupyter-notebook vae_benchmark.ipynb
+```
--- vae.py
+++ vae.py
@@ -11,31 +11,24 @@ from keras.models import Model, Sequential
 
 def nll(y_true, y_pred):
     """ Negative log likelihood (Bernoulli). """
-
     # keras.losses.binary_crossentropy gives the mean
     # over the last axis. we require the sum
     return K.sum(K.binary_crossentropy(y_true, y_pred), axis=-1)
-
 
 class KLDivergenceLayer(Layer):
     """ Identity transform layer that adds KL divergence
     to the final model loss.
     """
-
     def __init__(self, *args, **kwargs):
         self.is_placeholder = True
         super(KLDivergenceLayer, self).__init__(*args, **kwargs)
 
     def call(self, inputs):
-
         mu, log_var = inputs
-
         kl_batch = - .5 * K.sum(1 + log_var -
                                 K.square(mu) -
                                 K.exp(log_var), axis=-1)
-
         self.add_loss(K.mean(kl_batch), inputs=inputs)
-
         return inputs
 
 class VAE():
